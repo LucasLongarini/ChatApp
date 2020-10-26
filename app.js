fc5612e1b746e8afeececdc241b55538142cdf07
Lucas Longarini <lucaslongarini0@gmail.com>
2020-10-26 14:36:45 -0600

    init commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Server Listening on port " + port)
+})
+
+app.get('/', (req, res) => {
+    res.status(200).json({ response: "Root" })
+})
+
+//Errors 
+app.use((req, res, next) => {
+    const error = new Error('Not Found')
+    error.status(404)
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({ error: error.message })
+})
